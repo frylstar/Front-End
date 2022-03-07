c883484897df1c336bb5bcd145d913828c392ba9
frylstar <chengxiaszz@sina.com>
2022-03-07 23:15:16 +0800

    initial

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,16 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  // 站点配置
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  
+  base: '/front-end/',
+  
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+})
